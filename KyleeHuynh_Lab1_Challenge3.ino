d06d4576086b8b97d9a5ebe52897f2437ed22dde
kah008 <123135491+kah008@users.noreply.github.com>
2023-02-01 23:41:38 -0800

    Add files via upload

---
+++ KyleeHuynh_Lab1_Challenge3.ino
@@ -0,0 +1,177 @@
+#include <Servo.h>
+#include <IRremote.h>
+
+#define ON_OFF_KEY 0xFF629D
+#define TOGGLE_COLOR_KEY 0xFF30CF
+#define BRIGHTNESS_UP_KEY 0xFF9867
+#define BRIGHTNESS_DOWN_KEY 0xFF38C7
+#define CIRCLE_KEY 0xFF18E7
+#define CUSTOM_FUNCTION_KEY_A 0xFF22DD
+#define CUSTOM_FUNCTION_KEY_B 0xFF02FD
+#define CUSTOM_FUNCTION_KEY_C 0xFFC23D
+
+Servo myservo; // create servo object to control a servo
+const int servoPin = 9;
+int angle = 0;
+
+const int RECV_PIN = 11;
+const int redPin = 5;
+const int greenPin = 4;
+const int bluePin = 3;
+int red = 0;  // Initial intensity
+int green = 0;
+int blue = 0;
+int currentBrightness = 0;  // Initial active color intensity
+String targetColor = "RED"; // Initial active color
+bool isOn = false;
+IRrecv irrecv(RECV_PIN);  // Initialize IR Library
+decode_results results;   // Initialize IR Library
+
+void setup() {
+  Serial.begin(9600);   // Start Serial
+  irrecv.enableIRIn();  // Start the receiver
+  // Set the three LED Pins as outputs
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  myservo.attach(servoPin);
+}
+void loop() {
+  if (irrecv.decode(&results)) {
+    // Serial.println(results.value, HEX);  // Prints HEX code IR receiver receives
+    irrecv.resume(); // Receive the next value
+    // Power switch
+    if (results.value == ON_OFF_KEY) {
+      if (isOn == true) {
+        Serial.println("Turning Off"); powerOff();
+      }
+      else if (isOn == false) {
+        Serial.println("Turning On"); powerOn(); showCurrentActiveColor();
+      }
+    }
+    // Color operations
+    if (isOn == true) {
+      if (results.value == TOGGLE_COLOR_KEY)      // Toggle active colors
+        toggleActiveColor();
+      else if (results.value == BRIGHTNESS_UP_KEY) // Brightness up
+        brightnessUp();
+      else if (results.value == BRIGHTNESS_DOWN_KEY) // Brightness down
+        brightnessDown();
+      else if (results.value == CIRCLE_KEY) // Circle
+        circle();
+      else if (results.value == CUSTOM_FUNCTION_KEY_A) {
+        servoReset();
+      }
+      else if (results.value == CUSTOM_FUNCTION_KEY_B) {
+        servoShort();
+        brightnessUp();
+      }
+      else if (results.value == CUSTOM_FUNCTION_KEY_C) {
+        servoLong();
+        brightnessDown();
+      }
+    }
+  }
+  delay(100);
+}
+// Print current color setting on serial monitor
+void showCurrentActiveColor() {
+  Serial.print("Now controlling color ");
+  Serial.println(targetColor);
+}
+// Turn Off LED
+void powerOff() {
+  analogWrite(redPin, 0);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 0);
+  isOn = false;
+}
+// Turn on LED
+void powerOn() {
+  updateColor();
+  isOn = true;
+}
+// Turn up brightness for active color
+void brightnessUp() {
+  currentBrightness = (currentBrightness + 25) % 256;
+  updateColor();
+}
+// Turn down brightness for active color
+void brightnessDown() {
+  currentBrightness = max(0, (currentBrightness - 25) % 256);
+  updateColor();
+}
+// Toggle active color that you are changing brightness on
+void toggleActiveColor() {
+  if (targetColor == "RED")
+    targetColor = "GREEN";
+  else if (targetColor == "GREEN")
+    targetColor = "BLUE";
+  else if (targetColor == "BLUE")
+    targetColor = "RED";
+  showCurrentActiveColor();
+}
+// Update LED color to current setting
+void updateColor() {
+  if (targetColor == "RED")
+    red = currentBrightness;
+  else if (targetColor == "GREEN")
+    green = currentBrightness;
+  else if (targetColor == "BLUE")
+    blue = currentBrightness;
+  setColor(red, green, blue);
+  showColorOnSerialMonitor();
+}
+// Display current color in Serial Monitor
+void showColorOnSerialMonitor() {
+  Serial.print("Red ");
+  Serial.print(red / 255.0);
+  Serial.print("\tGreen ");
+  Serial.print(green / 255.0);
+  Serial.print("\tBlue ");
+  Serial.println(blue / 255.0);
+}
+// Output desired voltages to LED
+void setColor(int red, int green, int blue) {
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+}
+// Circle through different colors
+void circle() {
+  red = 255;
+  green = 127;
+  blue = 0;
+  Serial.println("Start Circle");
+  while (true) {
+    red = (red + 25 + 256) % 256;
+    green = (green + 25 + 256) % 256;
+    blue = (blue + 25 + 256) % 256;
+    setColor(red, green, blue);
+    showColorOnSerialMonitor();
+    delay(150);
+    if (irrecv.decode(&results)) {
+      Serial.println("Stop Circle");
+      irrecv.resume();
+      return;
+    }
+  }
+}
+
+void servoReset() {
+  myservo.write(0);
+  Serial.println("Servo resetting to 0 degrees.");
+  delay(500);
+}
+
+void servoShort() {
+  myservo.write(180);
+  Serial.println("Servo rotated to 90 degrees.");
+  delay(500);
+}
+
+void servoLong() {
+  myservo.write(270);
+  Serial.println("Servo rotated to 180 degrees.");
+  delay(500);
+}
